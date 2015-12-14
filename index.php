6c788c81a88ce5e5f45e325e0e431f135ff5de9c
Sara Johnston <sarajohnston4@gmail.com>
2015-12-13 21:25:36 -0800

    header layout drafted, navbar drafted, responsive/media queries

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Book club</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div>
+    <?php
+    include_once('navbar.php');
+    ?>
+
+    <?php
+    include_once('footer.php');
+    ?>
+</div>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,27 @@
+<?php
+$nav = array(
+    "book_clubs" => array('link_text' => 'Book Clubs', 'link_url' => 'book_clubs.php'),
+    "book_lists" => array('link_text' => 'Book Lists', 'link_url' => 'book_lists.php'),
+    "book_nooks" => array('link_text' => 'Book Nooks', 'link_url' => 'book_nooks.php'),
+    "calendar" => array('link_text' => 'Calendar', 'link_url' => 'calendar.php')
+);
+?>
+
+<div class="nav_container">
+    <img class="logo" src="images/bookniche_logo.png">
+    <div class="nav_div">
+        <ul class="nav_ul">
+            <?php
+            foreach ($nav as $key => $value) {
+                ?>
+                <li class="nav_li">
+                    <a href="index.php?page=<?=$key;?>"><?=$value['link_text'];?></a>
+                </li>
+                <?php
+            }
+            ?>
+        </ul>
+    </div>
+
+</div>
+
